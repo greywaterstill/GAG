465a117e64af60301688c830b793d23072c35ba3
greywaterstill <calebpinkerton0@gmail.com>
2025-06-17 12:38:47 -0500

    Update GAG.lua

--- GAG.lua
+++ GAG.lua
@@ -1,3 +1,10 @@
-spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/base.lua"))();
-end)
+
+if identifyexecutor and identifyexecutor():lower():find("delta") then
+    print("Delta")
+else
+    spawn(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
+    end)
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))();
