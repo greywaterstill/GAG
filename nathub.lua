3ea3e4837b24095c55c49cfc525c8ee8c478488a
greywaterstill <calebpinkerton0@gmail.com>
2025-07-23 01:10:04 -0500

    Update nathub.lua

--- nathub.lua
+++ nathub.lua
@@ -2,9 +2,6 @@
 if identifyexecutor and identifyexecutor():lower():find("delta") then
     print("Delta")
 else
-    spawn(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
-    end)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))();
