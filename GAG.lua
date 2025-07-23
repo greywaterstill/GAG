8692947faf2d1c4197ee989227499a76047f7ce0
greywaterstill <calebpinkerton0@gmail.com>
2025-07-22 22:28:39 -0500

    Update GAG.lua

--- GAG.lua
+++ GAG.lua
@@ -5,6 +5,8 @@ else
     spawn(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/base.lua"))();
     end)
+    spawn(function()
+        task.wait(60)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/autotest.lua"))();
+    end)
 end
-
-
