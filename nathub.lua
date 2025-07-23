b7455acc6fc3774ac42a3163f61b45da3c5241e6
greywaterstill <calebpinkerton0@gmail.com>
2025-07-23 01:11:40 -0500

    Update nathub.lua

--- nathub.lua
+++ nathub.lua
@@ -1,7 +1,1 @@
-
-if identifyexecutor and identifyexecutor():lower():find("delta") then
-    print("Delta")
-else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
-end
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
